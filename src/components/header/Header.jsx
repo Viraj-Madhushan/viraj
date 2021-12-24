0dd9c23788d01bb3773600463a9dfc73659019b1
Viraj-Madhushan <viraj.madhushan.971210@gmail.com>
2021-12-24 10:34:14 +0530

    Header Completed

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHome,
+  faAddressBook,
+  faInfoCircle,
+  faWrench,
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div className="h">
+      <div className="h-menu">
+        <Link to="/">
+          <FontAwesomeIcon icon={faHome} className="h-icon" />
+        </Link>
+
+        <Link to="/about">
+          <FontAwesomeIcon icon={faInfoCircle} className="h-icon" />
+        </Link>
+
+        <Link to="/productList">
+          <FontAwesomeIcon icon={faWrench} className="h-icon" />
+        </Link>
+
+        <Link to="/contact">
+          <FontAwesomeIcon icon={faAddressBook} className="h-icon" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
